43582291a4b74d75d07b9fca46f938fe01c11b3b
Sergio Soares <se.rasoares@gmail.com>
2018-09-17 13:01:06 -0300

    criacao do projeto

---
+++ src/test/java/com/serviconline/application/ServiconlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serviconline.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiconlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
